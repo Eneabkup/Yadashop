ef50eb3bd3792898c13da023628623309f92bfc8
Waroon Suppakit <waroonsuppakit@Waroons-MacBook-Air.local>
2020-09-11 23:47:31 +0700

    add condition

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -3,22 +3,13 @@ import { userCollection , equipmentCollection} from '../firebase'
 import '../css/AdminPage.css';
 
 export default function AdminPage(){
+    
+    //set database user
     const [username , setUsername] = useState("")
     const [password , setPassword] = useState("")
     const [nameUser , setNameUser] = useState("")
 
-    
-    const [nameEquipment , setNameEquipment] = useState("")
-    const [amount , setAmount] = useState("")
-    const [price , setPrice] = useState("")
-    const [status , setStatus] = useState(false)
-
-
-    const [condition , setCondition] = useState("")
-
-
-     // database user
-    const setUsers = (e) => {
+    const setUsers = () => {
         if(username == "" || password == "" || nameUser == ""){
             window.alert("Please try again")
             return false
@@ -34,8 +25,16 @@ export default function AdminPage(){
         return true
     }
 
-    // database equipment
-    const setEquipment = (e) => {
+
+
+
+    //set database equipment
+    const [nameEquipment , setNameEquipment] = useState("")
+    const [amount , setAmount] = useState("")
+    const [price , setPrice] = useState("")
+    const [status , setStatus] = useState(false)
+
+    const setEquipment = () => {
         if(nameEquipment == "" || amount == "" || price == "" || isNaN(parseInt(amount)) || isNaN(parseFloat(price))){
             window.alert("Please try again")
             setNameEquipment("")
@@ -60,13 +59,96 @@ export default function AdminPage(){
         return true
     }
 
-    const userAction = (e) => {
+
+    //read data
+    const [lists , setLists] = useState([])
+
+    const readUser = () => {
+        const tmpLists = []
+        userCollection.get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                const taskformat = {
+                    username : doc.id,
+                    password : doc.data().password,
+                    name : doc.data().name
+                }
+                tmpLists.push(taskformat)
+            });
+        });
+        setLists(tmpLists)
+    }
+
+    const readStock = () => {
+        const tmpLists = []
+        equipmentCollection.get()
+            .then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                const taskformat = {
+                    name : doc.id,
+                    amount : doc.data().amount,
+                    price : doc.data().price,
+                    status : doc.data().status
+                }
+                tmpLists.push(taskformat)
+            });
+        });
+        setLists(tmpLists)
+    }
+
+
+
+    //set page
+    const [condition , setCondition] = useState("")
+
+    const userAction = () => {
         setCondition("User")
     }
 
-    const stockAction = (e) => {
+    const stockAction = () => {
         setCondition("Stock")
     }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     //User
     if(condition == "User"){
         return (
@@ -79,13 +161,16 @@ export default function AdminPage(){
 		                <h1 class="heading-primary">
 		    	            <span class="heading-primary-main">Management</span>
 		    	            <span class="heading-primary-sub">User</span>
+		                </h1>
+                        <a>test</a>
+                        <h1>
                             <form>
-                                <input type="text" class="form_user" id="Username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required=""/>
-                                <input type="password" class="form_user" id="Password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required=""/>
-                                <input type="text" class="form_user" id="Name" placeholder="Name" value={nameUser} onChange={(e) => setNameUser(e.target.value)} required=""/>
+                                <input type="text" class="input-admin" id="Username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required=""/>
+                                <input type="password" class="input-admin" id="Password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required=""/>
+                                <input type="text" class="input-admin" id="Name" placeholder="Name" value={nameUser} onChange={(e) => setNameUser(e.target.value)} required=""/>
                                 <div><a href="#" class="btn btn-white btn-animated" onClick = {setUsers}>Add/Update</a></div>
                             </form>
-		                </h1>
+                        </h1>
 		                <a href="#" class="btn btn-white btn-animated" onClick = {userAction}>User</a>
                         <a href="#" class="btn btn-white btn-animated" onClick = {stockAction}>Stock</a>
                         <a href="/" class="btn btn-white btn-animated">Logout</a>
@@ -105,14 +190,17 @@ export default function AdminPage(){
 	                <div class="text-box">
 		                <h1 class="heading-primary">
 		    	            <span class="heading-primary-main">Management</span>
-		    	            <span class="heading-primary-sub">Stock</span>
+		    	            <span class="heading-primary-sub">Stock</span>    
+		                </h1>
+                            <a>sdfasdf</a>
+                        <h1>
                             <form>
-                                <input type="text" class="form_stock" id="Name" placeholder="Name" value={nameEquipment} onChange={(e) => setNameEquipment(e.target.value)} required=""/>
-                                <input type="text" class="form_stock" id="Price" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} required=""/>
-                                <input type="text" class="form_stock" id="Amount" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} required=""/>
+                                <input type="text" class="input-admin" id="Name" placeholder="Name" value={nameEquipment} onChange={(e) => setNameEquipment(e.target.value)} required=""/>
+                                <input type="text" class="input-admin" id="Price" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)} required=""/>
+                                <input type="text" class="input-admin" id="Amount" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} required=""/>
                                 <div><a href="#" class="btn btn-white btn-animated" onClick = {setEquipment}>Add/Update</a></div>
                             </form>
-		                </h1>
+                        </h1>
 		                <a href="#" class="btn btn-white btn-animated" onClick = {userAction}>User</a>
                         <a href="#" class="btn btn-white btn-animated" onClick = {stockAction}>Stock</a>
                         <a href="/" class="btn btn-white btn-animated">Logout</a>
