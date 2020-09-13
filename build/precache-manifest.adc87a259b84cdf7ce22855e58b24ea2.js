953f13eaa84f1f630e55c4057ff3a2f66840cdd3
Waroon Suppakit <waroonsuppakit@Waroons-MacBook-Air.local>
2020-09-13 16:45:39 +0700

    add yarn

---
+++ build/precache-manifest.adc87a259b84cdf7ce22855e58b24ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a37af48461925adeba3573074acfa2",
+    "url": "/yadashop/index.html"
+  },
+  {
+    "revision": "3df2ed41105b85038aa9",
+    "url": "/yadashop/static/css/main.48c8669d.chunk.css"
+  },
+  {
+    "revision": "0d9c4033216156065322",
+    "url": "/yadashop/static/js/2.69bafc87.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/yadashop/static/js/2.69bafc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3df2ed41105b85038aa9",
+    "url": "/yadashop/static/js/main.8deb7847.chunk.js"
+  },
+  {
+    "revision": "ab4cf2f1be2b862e23f1",
+    "url": "/yadashop/static/js/runtime-main.e29ba48a.js"
+  }
+]);
