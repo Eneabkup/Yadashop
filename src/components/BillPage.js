6273eb42376b04c73016dd4e7f56e0b187e4c52d
Waroon Suppakit <waroonsuppakit@Waroons-MacBook-Air.local>
2020-11-20 06:51:19 +0700

    Add page

---
+++ src/components/BillPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/main.css';
+
+
+export default function CheckOrderPage(props){
+    
+    return (
+        <div>
+            <body class="body">
+                <center>
+                    <h1 class="heading-primary">
+                        <span class="heading-primary-main">Bill</span>
+                    </h1>
+                </center>
+                <div class="text-box">
+                    <a href="/" class="btn btn-white btn-animated">Back</a>
+                </div>
+            </body>
+        </div>
+    );
+}
---
+++ src/components/InputCustomerPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../css/main.css';
+
+
+export default function InputCustomer(props){
+    
+    return (
+        <div>
+            <body class="body">
+                <center>
+                    <h1 class="heading-primary">
+                        <span class="heading-primary-main">Information Customers</span>
+                    </h1>
+                </center>
+                <div class="text-box">
+                    <a href="/" class="btn btn-white btn-animated">Back</a>
+                </div>
+            </body>
+        </div>
+    );
+}
