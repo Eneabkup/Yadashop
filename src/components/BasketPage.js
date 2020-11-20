0f4f6b8496791f39b7dcd25a6fd4741c412f6843
Waroon Suppakit <waroonsuppakit@Waroons-MacBook-Air.local>
2020-11-21 04:29:59 +0700

    Add Success

--- src/components/BasketPage.js
+++ src/components/BasketPage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { order , customer , detail ,  product} from '../firebase'
-import { Redirect } from "react-router-dom";
 import '../css/main.css';
 
 
